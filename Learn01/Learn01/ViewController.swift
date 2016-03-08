460f92752467967046f2d5ff4881e69a346d1268
fly06102559 <409583168@qq.com>
2016-03-08 12:57:45 +0800

    init
    init learn01

---
+++ Learn01/Learn01/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Learn01
+//
+//  Created by Liu Wei on 16/3/8.
+//  Copyright © 2016年 Liu Wei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
